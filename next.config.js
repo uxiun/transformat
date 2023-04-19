a14a2c1f5161112611eaec043d9491d6209168d4
uxiun <teppeiito0905@gmail.com>
2023-04-19 23:26:04 +0900

    set urlprefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : undefined // if "" instead of undefined: error
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  assetPrefix: urlPrefix,
+  basePath: urlPrefix,
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
